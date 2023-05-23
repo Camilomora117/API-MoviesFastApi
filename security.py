3f00a9b8eba2377533d87e4b1baacb77ffd4d67a
camilomora117 <yesid.mora@mail.escuelaing.edu.co>
2023-05-22 23:35:10 -0500

    Archivos

--- security.py
+++ security.py
@@ -5,9 +5,7 @@ from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.user import user_router
 from routers.order import order_router
-from pydantic import BaseModel
-from sqlalchemy import MetaData, Table
-
+from sqlalchemy import Table
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
@@ -20,7 +18,4 @@ app.include_router(order_router)
 
 @app.get('/', tags=['home'])
 def message():
-    table_name = 'order_movies'
-    table = Table(table_name, Base.metadata, autoload=True, autoload_with=engine)
-    table.drop(engine)
     return HTMLResponse('<h1>Hello world</h1>')
